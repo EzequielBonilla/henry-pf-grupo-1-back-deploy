ee36dc2f94bc354347f7584c69106303cad7998a
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2025-01-20 11:34:43 -0300

    test push

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { SeedManager } from './seeds/seed.manager';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
     origin: '*',
   });
-  
+
   setupSwagger(app);
 
   const seedManager = app.get(SeedManager);
@@ -18,3 +18,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+//
